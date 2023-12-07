0e8d12b483770f2647ca2090a23032b5e1a5674b
rhsok <rhsok823@gmail.com>
2023-12-07 16:28:15 +0900

    Docs: url로 정보 넘겨받음 - url에 jwt토큰 정보 받아서 프론트에서 받아옴

--- src/app/auth/google/callback/page.tsx
+++ src/app/auth/google/callback/page.tsx
@@ -1,29 +1,30 @@
 'use client'
 import Cookies from 'js-cookie'
+import { cookies } from 'next/headers'
 import { useEffect } from 'react'
 import axios from 'axios'
+import { useParams, useSearchParams } from 'next/navigation'
+import { useQueries } from 'react-query'
+// import { getServerSideProps } from 'next/dist/build/templates/pages'
 
 function Page() {
-  // const cookieValue = Cookies.get('jwt')
-  const GoogleURL = process.env.NEXT_PUBLIC_GOOGLE_URL
+  const params = useSearchParams()
+  useEffect(() => {
+    const jwt = params.jwt
+    console.log(jwt)
+    if (!jwt) return
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get('https://test.qaing.co/auth/check', {
-  //         withCredentials: true,
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //       })
-  //       const data = response.data
-  //       console.log(data)
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error)
-  //     }
-  //   }
-  //   fetchData()
-  // }, [])
+    Cookies.set('access-token', jwt)
+    axios
+      .get('https://test.qaing.co/auth/goole/check', {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      })
+      .then(res => {
+        console.log(res)
+      })
+  }, [params])
 
   return <div>page</div>
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import Cookies from 'js-cookie'
 
-export default function middleware(res: NextResponse) {
-  console.log('res', res)
-  const userdata = res.headers.get('authorization')
-  if (userdata) {
-    Cookies.set('authorization', userdata, { expires: 1 })
-  }
-
+export default function middleware(req: NextRequest, res: NextResponse) {
   return NextResponse.next()
 }
