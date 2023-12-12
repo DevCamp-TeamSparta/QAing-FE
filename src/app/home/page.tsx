5693ad197bcc106c37fc951362a09bd4e4bfd286
rhsok <rhsok823@gmail.com>
2023-12-12 14:05:13 +0900

    Docs: home페이지 추가 - 레코드의 영상을 서버로 보내는 로직이 app.qaing에서만 동작하므로 녹화시작 버튼을 누르면 app.qaing.co/home 으로 이동 하도록 함

---
+++ src/app/home/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from '@/components/atoms/LogoAtoms'
+
+function Page() {
+  const logoSize = {
+    alt: 'Logo',
+    width: 100,
+    height: 36,
+  }
+  return (
+    <div>
+      <header className="h-[108px]   flex flex-col justify-center  ">
+        {/* 헤더 */}
+        <div className="ml-[35px] flex justify-between felx-row  ">
+          <Logo logoSize={logoSize} />
+        </div>
+      </header>
+      <div className="flex flex-row justify-center items-center h-[640px] t1">
+        여기서 녹화를 시작해 주세요
+      </div>
+    </div>
+  )
+}
+
+export default Page
