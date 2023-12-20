27a33b792dd3fcabd2a5cd49592ee7a3d40299ac
rhsok <117060851+rhsok@users.noreply.github.com>
2023-12-20 22:08:35 +0900

    Feat: issue명 수정, issue 삭제 api연결 로직 작성 - src/services/issue/issue.api.ts

---
+++ src/services/issue/issue.api.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import instance from '@/services/instance'
+
+export const editIssue = async (
+  folderId: string,
+  issueId: string,
+  UpdateIssueFileDto: object,
+) => {
+  const response = await instance.put(
+    `/folders/${folderId}/issues/${issueId}`,
+    UpdateIssueFileDto,
+  )
+  console.log('이슈명 수정 성공:', response)
+  return response.data
+}
+
+export const deleteIssue = async (folderId: string, issueId: string) => {
+  const response = await instance.delete(
+    `/folders/${folderId}/issues/${issueId}`,
+  )
+  console.log('이슈명 삭제:', response)
+  return response.data
+}
