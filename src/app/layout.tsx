aab77ab025be72be082a6b4c44789f5a8b994b3c
rhsok <117060851+rhsok@users.noreply.github.com>
2024-01-02 11:51:39 +0900

    Feat: 채널톡 추가 - 채널톡 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ModalPortal from '@/components/organisms/ModalOrganism/ModalPortalOrganism'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -47,6 +48,17 @@ export default function RootLayout({
         />
       </head>
       <body className=" w-screen flex flex-col items-center  ">
+        <Script
+          id="channelTalk"
+          dangerouslySetInnerHTML={{
+            __html: ` (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+
+ChannelIO('boot', {
+  "pluginKey": "e7347489-69c7-48e9-875b-92a53860f083"
+});`,
+          }}
+        />
+
         <div className=" box-borders w-full max-w-[1440px] font-pretendard">
           {children}
           <ModalPortal />
