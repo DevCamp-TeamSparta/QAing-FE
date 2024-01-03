b0cd1f2f437deb4e471b397e3336f1c5cf7e3b4a
rhsok <117060851+rhsok@users.noreply.github.com>
2024-01-03 10:03:10 +0900

    Feat: 익스텐션 설치 여부확인 기능 추가

--- src/components/templetes/(sidebar)/MainPageTemplete/MainPageTemplete.tsx
+++ src/components/templetes/(sidebar)/MainPageTemplete/MainPageTemplete.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import FolderTable from '@/components/organisms/MainPageOrganism/FolderTable'
 import { useVideoStore } from '@/states/videoStore'
 import InstallBanner from '@/components/organisms/MainPageOrganism/InstallBanner'
@@ -8,21 +8,39 @@ import { useRouter } from 'next/navigation'
 import { initAmplitude, logPageView, logEvent } from '@/lib/amplitude'
 
 export default function MainPageTemplate() {
-  const router = useRouter()
-  const videos = useVideoStore(state => state.videos)
-  const getCookie = Cookies.get('access-token')
+  const [isExtensionInstalled, setIsExtensionInstalled] = useState(true)
 
   useEffect(() => {
     initAmplitude()
     logPageView('qaing_mainpage_view')
   }, [])
 
+  const extensionId = 'njjpnfbdgeihghedhhahflddfmfenfli'
+
+  async function checkExtensionInstalled() {
+    try {
+      const response = await fetch(`chrome-extension://${extensionId}/icon.png`)
+      return true
+    } catch (e) {
+      if (e instanceof TypeError && e.message == 'Failed to fetch') {
+        return false
+      } else {
+        throw e
+      }
+    }
+  }
+
   useEffect(() => {
-    // window.location.href = 'https://qaing.co'
-    // router.push('/')
-    const getCookie = Cookies.get('access-token')
-    // console.log('getCookie', getCookie)
-  }, [getCookie])
+    // Then in your site's buisness logic, do something like this
+
+    checkExtensionInstalled().then((installed: any) => {
+      if (installed) {
+        setIsExtensionInstalled(false)
+      } else {
+        console.log('The extension is NOT installed.')
+      }
+    })
+  }, [])
 
   return (
     <main className="flex flex-col w-[1172px] px-[36px]">
@@ -33,8 +51,12 @@ export default function MainPageTemplate() {
       >
         <h1 className={'h3'}>QA 폴더</h1>
       </header>
-      <InstallBanner />
-      <FolderTable />
+      {isExtensionInstalled && (
+        <InstallBanner isExtensionInstalled={isExtensionInstalled} />
+      )}
+      <div className={`${isExtensionInstalled ? '' : 'relative bottom-6'}`}>
+        <FolderTable />
+      </div>
     </main>
   )
 }
